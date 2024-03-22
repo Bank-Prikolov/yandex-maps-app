6eb5665ff4f03bab221ca7bc7323063ef6addf03
Alexandr <mikhalchenkov22@gmail.com>
2024-03-22 12:36:08 +0300

    requirements

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print('MAPS')
+import sys
+import json
+
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QMessageBox,
+    QRadioButton,
+)
+
+import requests
